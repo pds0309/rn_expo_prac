d680e5cc0ebb81f78c1a75ae146313cde02e0d0c
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-06-14 19:24:46 +0900

    feat: add notifcation receive event handler

--- notification/App.js
+++ notification/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
 import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
 
 Notifications.setNotificationHandler({
   // 알림을 받을때 트리거된다.
@@ -14,6 +15,19 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
+  useEffect(() => {
+    // 알림을 수신했을 때 이벤트 핸들러
+    const sub = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log(notification);
+        const username = notification.request.content.data.username;
+        console.log(username);
+      }
+    );
+    return () => {
+      sub.remove();
+    };
+  }, []);
   const handleScheduleNotificationPress = () => {
     Notifications.scheduleNotificationAsync({
       content: {
