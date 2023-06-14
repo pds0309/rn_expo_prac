534b81c88a93b52cfe8f2601870590f21596cfa6
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-06-14 20:35:12 +0900

    feat: add push notification

--- notification/App.js
+++ notification/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, Platform, StyleSheet, Text, View } from "react-native";
 import * as Notifications from "expo-notifications";
 import { useEffect } from "react";
 
@@ -15,6 +15,37 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
+  // get user token for push notification
+  useEffect(() => {
+    async function configPushNotifications() {
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+      if (finalStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        Alert.alert("Permisson required!!");
+        return;
+      }
+      const token = await Notifications.getExpoPushTokenAsync();
+      console.log(token);
+      /**
+       * On Android 13, app users must opt-in to receive notifications via a permissions prompt automatically triggered by the operating system.
+       * This prompt will not appear until at least one notification channel is created.
+       * The setNotificationChannelAsync must be called before getDevicePushTokenAsync or getExpoPushTokenAsync to obtain a push token
+       */
+      if (Platform.OS === "android") {
+        console.log("setNotificationChannel");
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.DEFAULT,
+        });
+      }
+    }
+    configPushNotifications();
+  }, []);
+
   useEffect(() => {
     // 알림을 수신했을 때 이벤트 핸들러
     const subReceive = Notifications.addNotificationReceivedListener(
@@ -48,12 +79,28 @@ export default function App() {
       },
     });
   };
+
+  const handleSendPushNotification = () => {
+    fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        to: "ExponentPushToken[yourdevicetoken]",
+        title: "hello pussy",
+        body: "world",
+      }),
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Button
         title="Schedule Notification"
         onPress={handleScheduleNotificationPress}
       />
+      <Button title="Push!" onPress={handleSendPushNotification} />
       <StatusBar style="auto" />
     </View>
   );
