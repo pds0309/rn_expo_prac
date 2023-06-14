fa2c31926c1605c310cbbd572fb07c9e6a38902b
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-06-14 19:29:58 +0900

    feat: add local notification response receive event handler

--- notification/App.js
+++ notification/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
 import * as Notifications from "expo-notifications";
 import { useEffect } from "react";
 
@@ -17,15 +17,23 @@ Notifications.setNotificationHandler({
 export default function App() {
   useEffect(() => {
     // 알림을 수신했을 때 이벤트 핸들러
-    const sub = Notifications.addNotificationReceivedListener(
+    const subReceive = Notifications.addNotificationReceivedListener(
       (notification) => {
         console.log(notification);
         const username = notification.request.content.data.username;
         console.log(username);
       }
     );
+    const subResponseReceive =
+      Notifications.addNotificationResponseReceivedListener((notification) => {
+        console.log(notification);
+        const username =
+          notification.notification.request.content.data.username;
+        console.log(username);
+      });
     return () => {
-      sub.remove();
+      subReceive.remove();
+      subResponseReceive.remove();
     };
   }, []);
   const handleScheduleNotificationPress = () => {
